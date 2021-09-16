95f057e405af0e508334b0d4aacb4ce3a092ef10
RuinerAl <amanladla2000@gmail.com>
2021-09-16 21:32:06 +0530

    modified W & H of bars

--- src/components/AnimGround.jsx
+++ src/components/AnimGround.jsx
@@ -2,44 +2,66 @@ function AnimGround(props) {
 
     let arr = props.arr;
 
+    let heightFactor = 350 / props.max;
+
+    let size = arr.length;
+
+    let flag = false;
+
+    console.log("received");
+
+    console.log(props.max);
+
+    console.log(arr);
+
+    if (size > 25) {
+        flag = true;
+    }
+
+
     return (
-        <div className = "animContainer">
-        <div className='anim'>
+        <div className="animContainer">
+            <div className="anim align-content-center">
 
-            {arr.map((value, index) => {
-                return (
+                {arr.map((value, index) => {
+                        return (
 
-                    <div
-                        className={"align-items-center"}
-                        style={
-                            {
-                                height: arr.at(index) * 2 + 100,
-                                display: "flex",
-                                flexDirection: "column",
-                                alignContent: "center",
-                                marginTop: "10px"
-                            }
-                        }>
+                            <div
+                                key={index}
+                                className={"align-items-center"}
+                                style={
+                                    {
+                                        height: arr.at(index) * heightFactor + 100,
+                                        display: "flex",
+                                        flexDirection: "column",
+                                        alignContent: "center",
+                                        marginTop: "10px",
+                                    }
+                                }>
 
-                        <div>
-                            <p style={{ marginBottom: "0" }}>{value}</p>
-                        </div>
-
-                        <div style={{
-                            height: arr.at(index) * 2 + 50, width: "40px", display: "flex",
-                            justifyContent: "center",
-                        }}>
-                            <div style={{ height: arr.at(index) * 2 + 50, width: "30px", backgroundColor: "green" }}>
-
+                                <div style={{
+                                    height: arr.at(index) * heightFactor + 50,
+                                    width: 1000 / size,
+                                    display: "flex",
+                                    justifyContent: "center",
+                                }}>
+                                    <div style={{
+                                        height: arr.at(index) * heightFactor + 50,
+                                        width: 6000 / (size * 7),
+                                        backgroundColor: "lightgreen"
+                                    }}>
+                                        {!flag && <div>
+                                            <p style={{marginBottom: "0"}}>{value}</p>
+                                        </div>}
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                );
-            }
-            )
-            }
+                        );
+                    }
+                )
+                }
 
-        </div>
+            </div>
         </div>
     );
 }
