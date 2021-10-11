cfe5693f637081f5e4ea72eee41aea1ab147544f
Mudra Bhandari <mudrabhandari123@gmail.com>
2021-10-11 11:26:37 +0530

    Quick Sort Modifications

--- src/algorithms/QuickSort.js
+++ src/algorithms/QuickSort.js
@@ -3,8 +3,7 @@ let positionedIndexArr = [];
 async function QuickSort(arr, low, high, update, speed, setIsSorted, setInProgress){
     await QuickSortImpl(arr, low, high, update, speed);
     positionedIndexArr = [];
-    await new Promise(done => setTimeout(() => done(), speed));
-    update(arr,-1,-1,false,-1,positionedIndexArr);
+    update(arr,-1,-1,-1,false,positionedIndexArr);
     setIsSorted(true);
     setInProgress(false);
 }
@@ -13,7 +12,9 @@ async function QuickSortImpl(arr, low, high, update, speed){
     if(low < high){
         let index = await partition(arr, low, high, update, speed);
         await QuickSortImpl(arr, low, index-1, update, speed);
+        positionedIndexArr.push(low);
         await QuickSortImpl(arr, index+1, high, update, speed);
+        positionedIndexArr.push(index+1);
     }
 }
 
@@ -25,22 +26,28 @@ async function partition(arr, low, high, update, speed){
     for(let j = low; j <= high-1; j++){
         if(arr[j] <= pivot){
             i++;
+            console.log("Inside for loop ",i,j,arr[i],arr[j]);
             await new Promise(done => setTimeout(() => done(), speed));
             update(arr, i, j, high, true, positionedIndexArr);
             let temp = arr[i];
             arr[i] = arr[j];
-            arr[j] = temp;
-            
+            arr[j] = temp;   
+            update(arr, i, j, high, false, positionedIndexArr);
+        }
+        else{
+            await new Promise(done => setTimeout(() => done(), speed));
+            update(arr, i+1, j, high, false, positionedIndexArr);
         }
     }
     positionedIndexArr.push(i+1);
+    console.log("Outside For loop ",i,high,arr[i+1],arr[high]);
     await new Promise(done => setTimeout(() => done(), speed));
     update(arr, i+1, high, i+1, true,positionedIndexArr);
     let temp = arr[i+1];
     arr[i+1] = arr[high];
     arr[high] = temp;
     await new Promise(done => setTimeout(() => done(), speed));
-    update(arr,-1,-1,-1,false,positionedIndexArr);
+    update(arr, i+1, high, i+1, false, positionedIndexArr);
     return i+1;
 }
 
